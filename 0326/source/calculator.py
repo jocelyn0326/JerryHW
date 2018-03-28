54d7acf12255e43737ad229f8e72c18004f17908
jocelyn0326 <gyc1233@gmail.com>
2018-03-28 17:16:45 +0800

    Add 0326 HW
    0326HW

---
+++ 0326/source/calculator.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    result = a+b
+    return result
+
+def minus(a,b):
+    result = a-b
+    return result
+
+def mutiply(a,b):
+    result = a*b
+    return result
+
+def divide(a,b):
+    result = a/b
+    return result
